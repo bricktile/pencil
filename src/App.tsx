aaa7b6a6c378b800a19365aa0e8dde4206df85e2
xiaojingzhao <xiaojingzhao091@gmail.com>
2019-10-31 22:43:46 +0800

    feat: add lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://bricktile.dev/logo.png"
-          className="App-logo"
-          alt="logo"
-        />
-        <p>CONSTRUCTING</p>
-      </header>
-    </div>
-  );
-};
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img
+					src="https://bricktile.dev/logo.png"
+					className="App-logo"
+					alt="logo"
+				/>
+				<p>CONSTRUCTING</p>
+			</header>
+		</div>
+	)
+}
 
-export default App;
+export default App
